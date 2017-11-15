9eb3ed003abcfbe24d89d07ba3a70d762b640afd
A Hjerpe <adam.hjerpe@gmail.com>
2017-11-15 16:47:32 +0100

    Removed code from old lessons

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-Vue.component('task-list', {
-  template: `
-    <ul>
-      <task v-for="task in tasks">{{ task.description }}</task>
-    </ul>
-  `,
-  data() {
-    return {
-      tasks: [
-        { description: 'Go to the store', completed: true},
-        { description: 'Finish tutorial', completed: false},
-        { description: 'Cook food', completed: false},
-        { description: 'Play games', completed: true},
-        { description: 'Learn JS', completed: false},
-      ]
-    }
-  }
-})
-
-Vue.component('task', {
-  template: `<li><slot></slot></li>`
-})
 
 var app = new Vue ({
   el: '#app'
