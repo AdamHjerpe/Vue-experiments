d5e20581129623adaf4469be56195b760ffdc219
A Hjerpe <adam.hjerpe@gmail.com>
2017-11-15 15:55:29 +0100

    Init

---
+++ main.js
@@ -0,0 +1,27 @@
+//import Room from './app/views/room'
+Vue.component('task-list', {
+  template: `
+    <ul>
+      <task v-for="task in tasks">{{ task.description }}</task>
+    </ul>
+  `,
+  data() {
+    return {
+      tasks: [
+        { description: 'Go to the store', completed: true},
+        { description: 'Finish tutorial', completed: false},
+        { description: 'Cook food', completed: false},
+        { description: 'Play games', completed: true},
+        { description: 'Learn JS', completed: false},
+        
+      ]
+    }
+  }
+})
+Vue.component('task', {
+  template: '<li><slot></slot></li>'
+})
+
+var app = new Vue ({
+  el: '#app'
+})
