0368744c640a06300dc7380e9411757492ed92a9
A Hjerpe <adam.hjerpe@gmail.com>
2017-11-16 14:15:18 +0100

    End Modal lesson

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-
+Vue.component('modal',{
+  template: `
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>`
+})
 var app = new Vue ({
-  el: '#app'
+  el: '#app',
+  data: {
+    showModal: false
+  }
 })
